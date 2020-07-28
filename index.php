8daaf5a484b5c8290fdb96c8e1f7e53c94c52abf
acosta1Best <68802824+acosta1Best@users.noreply.github.com>
2020-07-28 21:20:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+  <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+        </head>
+
+        <title>
+          Hello World
+        </title>
+      </head>
+      <?php
+        $num01 = 10;
+        $num02 = 10;
+        
+        // The equation
+        if ($num01*$num02 == 100) {
+          echo("true");
+        }
+        else {
+          echo("false");
+        }
+      ?>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+  </html>
